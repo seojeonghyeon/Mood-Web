5e6845eaca89e2ae384e241072b248758995f54e
seojeonghyeon <seojeonghyeon0630@gmail.com>
2021-09-09 12:53:16 +0900

    create Mood-Web Server

---
+++ Mood-Web/src/test/java/com/mood/moodweb/MoodWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mood.moodweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
